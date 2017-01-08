0378122d57dee8e251d6970e332eca423408f37a
Arthur Silva <arthur.fsilva96@gmail.com>
2017-01-08 10:12:59 -0200

    Adjust translator.js

--- assets/js/yii.translator.js
+++ assets/js/yii.translator.js
@@ -1,3 +1,31 @@
-yii.t = function(category, message, params = []){
-    alert(category + message);
+yii.t = function(category, message, params = []) {
+    var messages = {
+        'contatos':  {
+            'mykey': 'hello'
+        },
+    };
+
+    var getMessage = function(category) {
+        var messages = findCategory(category);
+        if (messages) {
+            //found categories
+        }
+        getMessageAjax(category);
+
+        getMessage(category);
+    };
+
+    var findCategory = function(category) {
+        return messages.hasOwnProperty(category);
+    }
+
+    var getMessageAjax = function(category){
+
+    }
+
+    getMessage(category);
+
+    return {
+
+    }
 }
