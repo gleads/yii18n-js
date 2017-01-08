712824553152b29517884ccbc364892575d60c32
Arthur Silva <arthur.fsilva96@gmail.com>
2017-01-08 13:19:26 -0200

    Renamed Class

--- TranslatorModule.php
+++ TranslatorModule.php
@@ -4,8 +4,10 @@ namespace yac\yii18n;
 
 use yii\base\Module;
 
-class Module extends Module
+class TranslatorModule extends Module
 {
+    public $controllerNamespace = 'yac\yii18n\controllers';
+
     public function init()
     {
         parent::init();
