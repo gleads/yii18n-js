824baa0dafff4b307a70a0186cf15752eea5c440
Arthur Silva <arthur.fsilva96@gmail.com>
2017-01-08 10:12:36 -0200

    Added TranslatorController

---
+++ TranslatorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace yac\yii18n;
+
+use yii\base\Controller;
+
+class TranslatorController extends Controller
+{
+    public function actionIndex($category, $message, $params = [])
+    {
+        
+    }
+}
