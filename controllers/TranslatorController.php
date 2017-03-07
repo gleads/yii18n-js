0a59a205d719c188431b7abdae004c89c394dada
arthurfsilva <arthur.fsilva96@gmail.com>
2017-03-07 10:43:04 -0300

    Added docblock

--- controllers/TranslatorController.php
+++ controllers/TranslatorController.php
@@ -6,8 +6,17 @@ use yii\web\Controller;
 use yac\yii18n\Messages;
 use yii\i18n\PhpMessageSource;
 
+/**
+ *
+ * @author Arthur Silva <arthur.fsilva96@gmail.com
+ */
 class TranslatorController extends Controller
 {
+    /**
+     * Get all messages of category message
+     * @param  string $category  category name
+     * @return array  Array with all messages of category
+     */
     public function actionIndex($category)
     {
         \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
